817d1d56b541b4a2eaa88950250b644a0c17eab0
emiliah93 <93ruzive@gmail.com>
2023-10-07 16:32:31 -0700

    Static lib

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,24 +1,22 @@
-#include "main.h"	
-	/**
-	 * char *_strcpy - program copies the string pointed to by src
-	 * @dest: copy to
-	 * @src: copy from
-	 * Return: string value
-	 */
-	char *_strcpy(char *dest, char *src)
-	{
-	        	int a = 0;
-	        	int b = 0;
-	 
-	        	while (*(src + a) != '\0')
-	        	{
-	                    	a++;
-	        	}
-	        	for ( ; b < a ; b++)
-	        	{
-	                    	dest[b] = src[b];
-	        	}
-	        	dest[a] = '\0';
-	        	return (dest);
-	}
+#include "main.h"
 
+/**
+  *_strcpy - copy string
+  *@dest: destiny
+  *@src: source
+  *Return: dest pointer
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+		j++;
+	return (dest);
+}
