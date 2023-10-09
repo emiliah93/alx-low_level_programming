e0c73ab2b048e973f7349be799ec67b65a0d203c
emiliah93 <93ruzive@gmail.com>
2023-10-08 20:07:47 -0700

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,97 +1,94 @@
 #include "main.h"
-#include <stdlib.h>
-#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
-  *length - function to check
-  *if string contains only numbers
-  *@str: constant string
-  *
-  *Return: 1 if true else 0
-  */
-int length(const char *str)
+ * isNumber - checks if a string is a number
+ * @s: string to check
+ * Return: 1 if number, 0 if not
+ */
+int isNumber(char *s)
 {
 	int i;
 
-	i = 0;
-	while (*str++)
-	{
-		i++;
-	}
-	return (i);
+	for (i = 0; s[i]; i++)
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	return (1);
 }
 
 /**
-  *array - create an array
-  *@size: size of array
-  *
-  *Return: pointer to array
-  */
-
-char *array(int size)
+ * print_result - prints the result of the multiplication
+ * @result: array of integers
+ * @len: length of result
+ * Return: void
+ */
+void print_result(int *result, int len)
 {
-	char *arr;
-	int index;
+	int i;
 
-	arr = malloc(sizeof(char) * size);
-	if (arr == NULL)
-		exit(98);
-	for (index = 0; index < (size - 1); index++)
-		arr[index] = 'x';
-	arr[index] = '\0';
-	return (arr);
+	/* remove leading zeros */
+	for (i = 0; i < len && result[i] == 0; i++)
+		;
+
+	/* if all digits are 0, print 0 */
+	if (i == len)
+		printf("0");
+
+	/* print the result */
+	for (; i < len; i++)
+		printf("%d", result[i]);
+
+	printf("\n");
 }
 
 /**
-  *iterate_zeros - checks for characters which have leading zeros
-  *@str: string being checked
-  *
-  *Return: pointer
-  */
-
-char *iterate_zeros(char *str)
+ * main - multiplies two positive numbers
+ * @ac: number of arguments
+ * @av: array of arguments
+ * Return: 0
+ */
+int main(int ac, char **av)
 {
-	while (*str && *str == '0')
-		str++;
-	return (str);
-}
+	int i, j, num1_len, num2_len;
+	int *result;
 
-/**
-  *
-  */
-
-
-
-/**
-  * main - entry point
-  * @argc: arguement count
-  * @argv: arguement array / vector
-  *
-  *Return: product of arguements passed
-  */
-
-int main(int argc, char *argv[])
-{
-	unsigned long long mul, num1, num2;
-	char *num1_str, *num2_str;
-
-	if (argc != 3)
+	if (ac != 3 || !isNumber(av[1]) || !isNumber(av[2]))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1_str = argv[1];
-	num2_str = argv[2];
-	if (!valid_number(num1_str) || !valid_number(num2_str))
+
+	num1_len = strlen(av[1]);
+	num2_len = strlen(av[2]);
+
+	result = calloc(num1_len + num2_len, sizeof(int));
+	if (result == NULL)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = strtoull(num1_str, NULL, 10);
-	num2 = strtoull(num2_str, NULL, 10);
-	mul = MUL(num1, num2);
-	printf("%lld", mul);
-	_putchar('\n');
+
+	/* multiply each digit of num1 with num2 */
+	for (i = num1_len - 1; i >= 0; i--)
+	{
+		for (j = num2_len - 1; j >= 0; j--)
+		{
+			/* multiply each digit and add to result */
+			int mul = (av[1][i] - '0') * (av[2][j] - '0');
+
+			/* add to previous result */
+			int sum = result[i + j + 1] + mul;
+
+			/* update result */
+			result[i + j] += sum / 10;
+			result[i + j + 1] = sum % 10;
+		}
+	}
+
+	/* print result and free the allocated memory */
+	print_result(result, num1_len + num2_len);
+	free(result);
 	return (0);
 }
